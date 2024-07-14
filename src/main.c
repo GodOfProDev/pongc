79774c1bfdc2914294583528e7d0401f2eb42fbe
GodOfPro <1387ilia@gmail.com>
2024-07-14 09:55:10 +0330

    Some refactoring!

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
+#include <math.h>
 #include <SDL2/SDL.h>
-
 #include <stdio.h>
 #include <stdbool.h>
 
@@ -31,24 +31,40 @@ typedef struct {
     Ball ball;
 
     bool isRunning;
-} State;
+} GameState;
 
 bool init();
 
+void initPaddles(GameState *state);
+
+void initBall(GameState *state);
+
 void cleanup();
 
-State *create_state(SDL_Window *window, SDL_Renderer *render);
+GameState *create_state(SDL_Window *window, SDL_Renderer *render);
+
+void renderLoop();
 
 void renderPaddles();
 
 void renderBall();
 
+void handleEvents();
+
 void handlePlayerInput(SDL_KeyCode key);
+
+void drawCircle(SDL_Renderer *renderer, int centerX, int centerY, float radius);
+
+bool checkCollision(Paddle *p, Ball *b);
+
+void updateBall();
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
+const int SCREEN_FPS = 60;
+const int SCREEN_TICKS_PER_FRAME = 1000 / SCREEN_FPS;
 
-State *gState = NULL;
+GameState *gState = NULL;
 
 int main(int argc, char **argv) {
     if (!init()) {
@@ -56,27 +72,17 @@ int main(int argc, char **argv) {
         return -1;
     }
 
-    SDL_Event e;
     while (gState->isRunning) {
-        while (SDL_PollEvent(&e) != 0) {
-            if (e.type == SDL_QUIT) {
-                gState->isRunning = false;
-            } else if (e.type == SDL_KEYDOWN) {
-                handlePlayerInput(e.key.keysym.sym);
-            }
+        int startTicks = SDL_GetTicks();
+
+        handleEvents();
+        updateBall();
+        renderLoop();
+
+        int delta = SDL_GetTicks() - startTicks;
+        if (delta < SCREEN_TICKS_PER_FRAME) {
+            SDL_Delay(SCREEN_TICKS_PER_FRAME - delta);
         }
-
-        if (gState->renderer == NULL) {
-            continue;
-        }
-
-        SDL_SetRenderDrawColor(gState->renderer, 0x00, 0x00, 0x00, 0xFF);
-        SDL_RenderClear(gState->renderer);
-
-        renderBall();
-        renderPaddles();
-
-        SDL_RenderPresent(gState->renderer);
     }
 
     cleanup();
@@ -118,41 +124,27 @@ void cleanup() {
     SDL_Quit();
 }
 
-State *create_state(SDL_Window *window, SDL_Renderer *render) {
-    State *state = malloc(sizeof(State));
+GameState *create_state(SDL_Window *window, SDL_Renderer *render) {
+    GameState *state = malloc(sizeof(GameState));
 
     state->window = window;
     state->renderer = render;
     state->isRunning = true;
 
-    Paddle p1;
-    p1.width = 10;
-    p1.height = 165;
-    p1.pos.y = (SCREEN_HEIGHT / 2.0) - (p1.height / 2.0);
-    p1.pos.x = p1.width + 16;
-    p1.speed = 10;
-
-    Paddle p2;
-    p2.width = 10;
-    p2.height = 165;
-    p2.pos.y = (SCREEN_HEIGHT / 2.0) - (p2.height / 2.0);
-    p2.pos.x = SCREEN_WIDTH - (p2.width + 32);
-    p2.speed = 10;
-
-    state->p1 = p1;
-    state->p2 = p2;
-
-    Ball ball;
-    ball.pos.x = 0;
-    ball.pos.y = 0;
-    ball.radius = 5;
-    ball.speed = 15;
-    ball.dir.x = -1;
-    ball.dir.y = 0;
-
-    state->ball = ball;
+    initPaddles(state);
+    initBall(state);
 
     return state;
+}
+
+void renderLoop() {
+    SDL_SetRenderDrawColor(gState->renderer, 0x00, 0x00, 0x00, 0xFF);
+    SDL_RenderClear(gState->renderer);
+
+    renderBall();
+    renderPaddles();
+
+    SDL_RenderPresent(gState->renderer);
 }
 
 void renderPaddles() {
@@ -169,49 +161,125 @@ void renderPaddles() {
 }
 
 void renderBall() {
+    SDL_SetRenderDrawColor(gState->renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+    Ball ball = gState->ball;
+    drawCircle(gState->renderer, ball.pos.x, ball.pos.y, ball.radius);
+}
+
+void handleEvents() {
+    SDL_Event e;
+    while (SDL_PollEvent(&e) != 0) {
+        if (e.type == SDL_QUIT) {
+            gState->isRunning = false;
+        } else if (e.type == SDL_KEYDOWN) {
+            handlePlayerInput(e.key.keysym.sym);
+        }
+    }
 }
 
 void handlePlayerInput(SDL_KeyCode key) {
-    Paddle p1;
-    Paddle p2;
-    bool isP1 = false;;
+    Paddle p1 = gState->p1;
+    Paddle p2 = gState->p2;
     switch (key) {
         case SDLK_w:
-            p1 = gState->p1;
             p1.pos.y -= p1.speed;
-            isP1 = true;
             break;
         case SDLK_s:
-            p1 = gState->p1;
             p1.pos.y += p1.speed;
-            isP1 = true;
             break;
+
         case SDLK_UP:
-            p2 = gState->p2;
             p2.pos.y -= p2.speed;
             break;
         case SDLK_DOWN:
-            p2 = gState->p2;
             p2.pos.y += p2.speed;
             break;
         default:
             return;
-            break;
     }
 
-    if (isP1) {
-        if (p1.pos.y < 0) {
-            p1.pos.y += p1.speed;
-        } else if (p1.pos.y + p1.height > SCREEN_HEIGHT) {
-            p1.pos.y -= p1.speed;
+    gState->p1 = p1;
+    gState->p2 = p2;
+}
+
+void drawCircle(SDL_Renderer *renderer, int centerX, int centerY, float radius) {
+    for (int x = centerX - radius; x <= centerX + radius; x++) {
+        for (int y = centerY - radius; y <= centerY + radius; y++) {
+            int dx = x - centerX;
+            int dy = y - centerY;
+            if ((dx * dx + dy * dy) <= (radius * radius)) {
+                SDL_RenderDrawPoint(renderer, x, y);
+            }
         }
-        gState->p1 = p1;
-    } else {
-        if (p2.pos.y < 0) {
-            p2.pos.y += p2.speed;
-        } else if (p2.pos.y + p2.height > SCREEN_HEIGHT) {
-            p2.pos.y -= p2.speed;
-        }
-        gState->p2 = p2;
     }
+}
+
+void updateBall() {
+    Ball ball = gState->ball;
+    Paddle p1 = gState->p1;
+    Paddle p2 = gState->p2;
+
+    ball.pos.x += ball.speed * ball.dir.x;
+    ball.pos.y += ball.speed * ball.dir.y;
+
+    if (checkCollision(&p1, &ball)) {
+        ball.dir.x = 1;
+        ball.dir.y = 0.25;
+    } else if (checkCollision(&p2, &ball)) {
+        ball.dir.x = -1;
+        ball.dir.y = -0.25;
+    }
+
+    gState->ball = ball;
+}
+
+bool checkCollision(Paddle *p, Ball *b) {
+    // Find the closest point on the paddle to the center of the ball
+    float closestX = fmaxf(p->pos.x, fminf(b->pos.x, p->pos.x + p->width));
+    float closestY = fmaxf(p->pos.y, fminf(b->pos.y, p->pos.y + p->height));
+
+    // Calculate the distance between the closest point and the ball's center
+    float distanceX = b->pos.x - closestX;
+    float distanceY = b->pos.y - closestY;
+
+    // Check if the distance is less than the ball's radius
+    float distanceSquared = (distanceX * distanceX) + (distanceY * distanceY);
+    return distanceSquared < (b->radius * b->radius);
+}
+
+void initPaddles(GameState *state) {
+    Paddle p1 = {
+        .width = 10,
+        .height = 165,
+        .speed = 10
+    };
+
+    p1.pos.y = (SCREEN_HEIGHT / 2.0) - (p1.height / 2.0);
+    p1.pos.x = p1.width + 16;
+
+    Paddle p2 = {
+        .width = 10,
+        .height = 165,
+        .speed = 10
+    };
+
+    p2.pos.y = (SCREEN_HEIGHT / 2.0) - (p2.height / 2.0);
+    p2.pos.x = SCREEN_WIDTH - (p2.width + 32);
+
+    state->p1 = p1;
+    state->p2 = p2;
+}
+
+void initBall(GameState *state) {
+    Ball ball = {
+        .radius = 13,
+        .speed = 2,
+    };
+
+    ball.pos.x = (SCREEN_WIDTH / 2.0);
+    ball.pos.y = (SCREEN_HEIGHT / 2.0);
+    ball.dir.x = -1;
+    ball.dir.y = 0;
+
+    state->ball = ball;
 }
